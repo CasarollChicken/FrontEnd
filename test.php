be784329998bfdbafa3b5bcbad20911ca688acae
CasarollChicken <casarollchicken@gmail.com>
2019-07-24 14:46:13 +1000

    Test Commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Suck it</title>
+  </head>
+  <body>
+
+     <form action="site.php" method="post">
+        Username: <input type="text" name="color"> <br>
+        Password: <input type="text & number" name="noun"> <br>
+        <input type="submit">
+     </form>
+     <br>
+     <?php
+     //GET is used for public url parameters
+     //While POST can be used more for security
+     ?>
+   </body>
+</html>
